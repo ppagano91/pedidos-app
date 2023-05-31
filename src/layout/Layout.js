7a772a2a4c344df3193870553a8884fb83b3b23c
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-31 09:11:33 -0300

    assets + componente Sidebar

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,3 +1,4 @@
+import Sidebar from "@/components/Sidebar";
 import Head from "next/head";
 export default function Home({ children, pagina }) {
   return (
@@ -8,7 +9,7 @@ export default function Home({ children, pagina }) {
       </Head>
       <div className="md:flex">
         <aside className="md:w-4/12 xl:w-1/4 2xl:1/5">
-          <h1>Sidebar</h1>
+          <Sidebar />
         </aside>
         <main className="md:w-8/12 xl:w-3/4 2xl:4/5 h-screen overflow-y-scroll">
           {children}
