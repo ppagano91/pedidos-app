4515a538ab867a89f5f0482ace2f67e4f60f2009
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-06-05 07:51:48 -0300

    corrección de importación

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import { PedidosProvider } from "@/context/PedidosProvide";
+import { PedidosProvider } from "@/context/PedidosProvider";
 
 export default function App({ Component, pageProps }) {
   return (
