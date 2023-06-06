1a45d2d0e4efb733aec9a93c8b034905c934a88d
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-06-06 09:31:23 -0300

    Mostrando los Productos de la Categoría actual

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -7,11 +7,11 @@ export default function Home({ children, pagina }) {
         <title>Café - {pagina}</title>
         <meta name="description" content="Café" />
       </Head>
-      <div className="md:flex">
-        <aside className="md:w-4/12 xl:w-1/4 2xl:1/5">
+      <div className="md:flex max-w-12xl mx-auto">
+        <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5">
           <Sidebar />
         </aside>
-        <main className="md:w-8/12 xl:w-3/4 2xl:4/5 h-screen overflow-y-scroll">
+        <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
           <div className="p-10">
             {children}
           </div>
