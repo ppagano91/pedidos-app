8fabaca6085c23d4b5f9d87cb5f0c50e28ce6304
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-07-10 09:46:04 -0300

    Registrando pedidos en DB

--- src/pages/api/ordenes.js
+++ src/pages/api/ordenes.js
@@ -1,16 +1,25 @@
-export default async function handler(req, res) {    
-    if (req.method === "POST") {
-        console.log(req.body)
-        res.json({ message: "You are posting" })
-    }
-    else{
-        res.json({ message: "You are not posting" })
-    }
+import { PrismaClient } from "@prisma/client";
 
-    // const ordenes = await prisma.orden.findMany({
-    //     include: {
-    //     productos: true,
-    //     },
-    // });
-    // res.status(200).json(ordenes);
-    }
+export default async function handler(req, res) {
+  const prisma = new PrismaClient();
+  if (req.method === "POST") {
+    const orden = await prisma.orden.create({
+      data: {
+        nombre: req.body.nombre,
+        total: req.body.total,
+        pedido: req.body.pedido,
+        fecha: req.body.fecha,
+      },
+    });
+    res.json(orden);
+  } else {
+    res.json({ message: "You are not posting" });
+  }
+
+  // const ordenes = await prisma.orden.findMany({
+  //     include: {
+  //     productos: true,
+  //     },
+  // });
+  // res.status(200).json(ordenes);
+}
