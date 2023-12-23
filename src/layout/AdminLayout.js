aba2b3f7412beef19f62ee02a1a935c3e852410e
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-12-23 00:27:09 -0300

    Agrego script db

--- src/layout/AdminLayout.js
+++ src/layout/AdminLayout.js
@@ -8,7 +8,7 @@ export default function AdminLayout({ children, pagina }) {
     <>
       <Head>
         <title>Café - {pagina}</title>
-        <meta name="description" content="Quosco Cafetería" />
+        <meta name="description" content="Quiosco Cafetería" />
       </Head>
 
       <div className="md:flex">
